4c36069ea7c21c9f3747d4643bdfde196542005d
codedbyshaun <shaun@codedbyshaun.com>
2023-10-13 01:54:42 +0200

    modify handleSubmit => bannedCountries as prop

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CreditCardInput from './CreditCardInput'
+import CountryInput from './CountryInput'
 
-const CardForm = () => {
-  return (
-    <div>CardForm</div>
-  )
+function CardForm({ onCardSubmit, bannedCountries }) {
+    const [cardNumber, setCardNumber] = useState('')
+    const [country, setCountry] = useState('')
+    const [errorMessage, setErrorMessage ] = useState('')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        if (bannedCountries.includes(country)) {
+            setErrorMessage('This country is banned')
+            return
+        }
+        if (cardNumber && country) {
+            onCardSubmit({ number: cardNumber, country: country })
+            setCardNumber('')
+            setCountry('')
+            setErrorMessage('')
+        }
+        
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CreditCardInput onCardNumberChange={setCardNumber} />
+            <CountryInput onCountryChange={setCountry} />
+            <button type="submit">Submit</button>
+            {errorMessage && <p>{errorMessage}</p>} {/* display error message if it exists */}
+        </form>
+    )
 }
 
-export default CardForm
+export default CardForm;
