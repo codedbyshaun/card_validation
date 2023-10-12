597c1fe5f6b280646aaffc5ad80f474290a45dd6
codedbyshaun <shaun@codedbyshaun.com>
2023-10-13 01:48:13 +0200

    complete component file structure + import to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import CardList from './components/CardList'
+import BannedCountries from './components/BannedCountries'
+import CardForm from './components/CardForm'
+import './App.css'
 
 function App() {
   return (
---
+++ src/components/BannedCountries.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BannedCountries = () => {
+  return (
+    <div>BannedCountries</div>
+  )
+}
+
+export default BannedCountries
