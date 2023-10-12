4820a5f9b5afdf0cc4fa3804850d1349aa5d5285
codedbyshaun <shaun@codedbyshaun.com>
2023-10-13 01:59:01 +0200

    add Valid Cards Saved section

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const CardList = () => {
-  return (
-    <div>CardList</div>
-  )
+function CardList({ cards }) {
+
+    function maskCardNumber(number) {
+        const len = number.length;
+        if (len >= 8) {
+            return number.slice(0, 4) + '*'.repeat(len - 8) + number.slice(len - 4);
+        } else {
+            return number;
+        }
+    }
+
+    return (
+        <div>
+            <h2>Valid Cards</h2>
+            <ul>
+                {cards.map((card) => (
+                    <li key={card.number}>
+                        Card Number: {(card.number)}
+                        <br />
+                        Country: {card.country}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default CardList
+export default CardList;
