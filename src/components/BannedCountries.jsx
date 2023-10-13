b9c26339806f2e52891ee759d988b3e27d903fe1
codedbyshaun <shaun@codedbyshaun.com>
2023-10-13 02:01:22 +0200

    add configurable Banned Countries function

--- src/components/BannedCountries.jsx
+++ src/components/BannedCountries.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BannedCountries = () => {
-  return (
-    <div>BannedCountries</div>
-  )
+function BannedCountries({ bannedCountries, onBannedCountriesChange }) { 
+    const [newCountry, setNewCountry] = useState('')
+
+    function handleAdd() {
+        if (newCountry && !bannedCountries.includes(newCountry)) {
+            onBannedCountriesChange([...bannedCountries, newCountry])
+            setNewCountry('')
+        }
+    }
+
+    function handleRemove(country) {
+        onBannedCountriesChange(bannedCountries.filter(c => c !== country))
+    }
+
+    return (
+        <div>
+            <h2>Banned Countries</h2>
+            <input type="text" value={newCountry} onChange={e => setNewCountry(e.target.value)} />
+            <button onClick={handleAdd}>Add</button>
+            <ul>
+                {bannedCountries.map(country => (
+                    <li key={country}>
+                        {country}
+                        <button onClick={() => handleRemove(country)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
-export default BannedCountries
+export default BannedCountries;
